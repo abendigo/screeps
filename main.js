38e41588a367e5b877e43749be8a8701ee6c7ad7
Mark Oosterveld <mark@oosterveld.org>
2016-11-19 14:50:45 -0500

    respawn

--- main.js
+++ main.js
@@ -6,7 +6,13 @@ module.exports.loop = function () {
 	for(var name in Game.rooms) {
         console.log('Room "'+name+'" has '+Game.rooms[name].energyAvailable+' energy');
     }
-    
+
+    var harvesters = _.filter(Game.creeps, (creep) => creep.memory.role == 'harvester');
+	var upgraders = _.filter(Game.creeps, (creep) => creep.memory.role == 'upgrader');
+	var builders = _.filter(Game.creeps, (creep) => creep.memory.role == 'builder');
+
+	console.log('harvestors', harvesters.length, 'upgraders', upgraders.length, 'builders', builders.length);
+
     for(var name in Game.creeps) {
         var creep = Game.creeps[name];
         if(creep.memory.role == 'harvester') {
@@ -21,3 +27,30 @@ module.exports.loop = function () {
     }
 }
 
+
+
+/*
+var roleHarvester = require('role.harvester');
+var roleUpgrader = require('role.upgrader');
+
+module.exports.loop = function () {
+
+    var harvesters = _.filter(Game.creeps, (creep) => creep.memory.role == 'harvester');
+    console.log('Harvesters: ' + harvesters.length);
+
+    if(harvesters.length < 2) {
+        var newName = Game.spawns['Spawn1'].createCreep([WORK,CARRY,MOVE], undefined, {role: 'harvester'});
+        console.log('Spawning new harvester: ' + newName);
+    }
+
+    for(var name in Game.creeps) {
+        var creep = Game.creeps[name];
+        if(creep.memory.role == 'harvester') {
+            roleHarvester.run(creep);
+        }
+        if(creep.memory.role == 'upgrader') {
+            roleUpgrader.run(creep);
+        }
+    }
+}
+*/
