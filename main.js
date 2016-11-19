dab7739f1a4992d57199e3493a1682f5fe08578e
Mark Oosterveld <mark@oosterveld.org>
2016-11-19 13:48:53 -0500

    rename, add some debugging

--- main.js
+++ main.js
@@ -3,7 +3,10 @@ var roleUpgrader = require('role.upgrader');
 var roleBuilder = require('role.builder');
 
 module.exports.loop = function () {
-
+	for(var name in Game.rooms) {
+        console.log('Room "'+name+'" has '+Game.rooms[name].energyAvailable+' energy');
+    }
+    
     for(var name in Game.creeps) {
         var creep = Game.creeps[name];
         if(creep.memory.role == 'harvester') {
