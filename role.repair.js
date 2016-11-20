e52a1ac30daacbb9761ad01beb22213b0a9271cb
Mark Oosterveld <mark@oosterveld.org>
2016-11-20 01:07:39 -0500

    repait it

--- role.repair.js
+++ role.repair.js
@@ -3,13 +3,34 @@ var roleRepair = {
 	/** @param {Creep} creep **/
     run: function(creep) {
 
-		var repairit = creep.room.find(FIND_STRUCTURES, { 
-		   filter: (structure) => { 
-		       return ((structure.hits < 5000) && (structure.hits > 0))
-		   }
-		});
+	    if(creep.memory.repair && creep.carry.energy == 0) {
+            creep.memory.repair = false;
+            creep.say('harvesting');
+	    }
+	    if(!creep.memory.repair && creep.carry.energy == creep.carryCapacity) {
+	        creep.memory.repair = true;
+	        creep.say('repair');
+	    }
 
-		console.log('to repair', repairit.length);
+
+	    if(creep.memory.repair) {
+			var targets = creep.room.find(FIND_STRUCTURES, { 
+			   filter: (structure) => { 
+			       return ((structure.hits < 5000) && (structure.hits > 0))
+			   }
+			});
+            if(targets.length) {
+                if(creep.repair(targets[0]) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(targets[0]);
+                }
+            }
+	    }
+	    else {
+	        var sources = creep.room.find(FIND_SOURCES);
+            if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(sources[0]);
+            }
+	    }
 	}
 };
 
