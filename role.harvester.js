2ccc79f8578b6ded236913fa62b799c3296ecd77
Mark Oosterveld <mark@oosterveld.org>
2016-11-20 14:45:42 -0500

    delivery better

--- role.harvester.js
+++ role.harvester.js
@@ -5,14 +5,17 @@ var roleHarvester = {
         if (creep.fatigue)
             return;
 
-
-	    if(creep.carry.energy < creep.carryCapacity) {
-            var sources = creep.room.find(FIND_SOURCES);
-            if(creep.harvest(sources[1]) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(sources[1]);
-            }
+        if(creep.memory.deliver && creep.carry.energy == 0) {
+            creep.memory.deliver = false;
+            creep.say('harvesting');
         }
-        else {
+        if(!creep.memory.deliver && creep.carry.energy == creep.carryCapacity) {
+            creep.memory.deliver = true;
+            creep.say('delivering');
+        }
+
+
+        if (creep.memory.deliver) {
             var targets = creep.room.find(FIND_STRUCTURES, {
                     filter: (structure) => {
                         return (structure.structureType == STRUCTURE_EXTENSION || structure.structureType == STRUCTURE_SPAWN) &&
@@ -23,6 +26,11 @@ var roleHarvester = {
                 if(creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
                     creep.moveTo(targets[0]);
                 }
+            }
+        } else {
+            var sources = creep.room.find(FIND_SOURCES);
+            if(creep.harvest(sources[1]) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(sources[1]);
             }
         }
 	}
