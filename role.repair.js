fcfe92fb4905ff37d1c81610c39b75381a286089
Mark Oosterveld <mark@oosterveld.org>
2016-11-20 01:02:41 -0500

    repair stuff

--- role.repair.js
+++ role.repair.js
@@ -1,11 +1,16 @@
 var roleRepair = {
-	var repairit = creep.room.find(FIND_STRUCTURES, { 
-	   filter: (structure) => { 
-	       return ((structure.hits < 5000) && (structure.hits > 0))
-	   }
-	});
 
-	console.log('to repair', repairit.length);
+	/** @param {Creep} creep **/
+    run: function(creep) {
+
+		var repairit = creep.room.find(FIND_STRUCTURES, { 
+		   filter: (structure) => { 
+		       return ((structure.hits < 5000) && (structure.hits > 0))
+		   }
+		});
+
+		console.log('to repair', repairit.length);
+	}
 };
 
 module.exports = roleRepair;
