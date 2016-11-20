9f66bce8bf20b164b71160f8f2bd70abeec1b02e
Mark Oosterveld <mark@oosterveld.org>
2016-11-20 15:32:48 -0500

    huh

--- role.builder.js
+++ role.builder.js
@@ -19,6 +19,7 @@ var roleBuilder = {
 	    }
 
 	    if(creep.memory.building) {
+	    	console.log('------ 1')
 	        var targets = creep.room.find(FIND_CONSTRUCTION_SITES);
             if(targets.length) {
                 if(creep.build(targets[0]) == ERR_NOT_IN_RANGE) {
@@ -40,6 +41,7 @@ var roleBuilder = {
             }
 	    }
 	    else {
+	    	console.log('------ 2')
 	        var sources = creep.room.find(FIND_SOURCES);
 	        console.log('source.length', sources.length)
             if(creep.harvest(sources[options.source]) == ERR_NOT_IN_RANGE) {
