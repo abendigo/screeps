e7866122245eee7898311961cbf7b8a9c4b8a71a
Mark Oosterveld <mark@oosterveld.org>
2016-11-20 20:15:55 -0500

    2

--- role.scout.js
+++ role.scout.js
@@ -19,9 +19,9 @@ var roleScout = {
         if (creep.memory.deliver) {
         	var x = 'W63S24';
 
-        	if (creep.room.name != 'x') {
-		    	creep.moveTo(Game.flags.Flag2);
-        	} else {
+       //  	if (creep.room.name != 'x') {
+		    	// creep.moveTo(Game.flags.Flag2);
+       //  	} else {
 
 	            var targets = Game.rooms[x].find(FIND_STRUCTURES, {
 	                    filter: (structure) => {
@@ -34,19 +34,23 @@ var roleScout = {
 	                    creep.moveTo(targets[0]);
 	                }
 	            }
-	        }
+	        // }
         } else {
 
 			var roomName = 'W63S23';
-	    	var sources = Game.rooms[roomName].find(FIND_SOURCES);
-	    	console.log('sources', sources.length);
 
-	        // var sources = creep.room.find(FIND_SOURCES);
-	        if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
-	            creep.moveTo(sources[0]);
-	        }
-	    }
+	    	if (creep.room.name != 'roomName') {
+		    	creep.moveTo(Game.flags.Flag2);
+		    } else {
+		    	var sources = Game.rooms[roomName].find(FIND_SOURCES);
+		    	console.log('sources', sources.length);
 
+		        // var sources = creep.room.find(FIND_SOURCES);
+		        if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
+		            creep.moveTo(sources[0]);
+		        }
+		    }
+		}
 	}
 };
 
