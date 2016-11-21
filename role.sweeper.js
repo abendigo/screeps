d31afed59654c57ec793ba6e84f13e7353ead976
Mark Oosterveld <marko@oanda.com>
2016-11-21 16:53:10 -0500

    deliver

--- role.sweeper.js
+++ role.sweeper.js
@@ -3,15 +3,35 @@ var roleUpgrader = {
     /** @param {Creep} creep **/
     run: function(creep, options) {
 
-        var litter = creep.room.find(FIND_DROPPED_ENERGY);
-        if (litter.length) {
-            creep.say('litter bug');
+	    if (creep.memory.deliver && creep.carry.energy == 0) {
+            creep.memory.deliver = false;
+            creep.say('sweeping');
+	    }
+	    if (!creep.memory.deliver && creep.carry.energy == creep.carryCapacity) {
+	        creep.memory.deliver = true;
+	        creep.say('deliver');
+	    }
 
-            if (creep.pickup(litter[0]) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(litter[0]);
+        if (creep.memery.deliver) {
+            var container = creep.pos.findClosestByRange(FIND_STRUCTURES, {
+                filter: (structure) => {
+                    return structure.structureType == 'container';
+                }
+            });
+
+            if (creep.transfer(container, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(container);
             }
         } else {
-
+            var litter = creep.room.find(FIND_DROPPED_ENERGY);
+            if (litter.length) {
+                if (creep.pickup(litter[0]) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(litter[0]);
+                }
+            } else {
+                creep.say('no litter');
+                creep.memery.deliver = true;
+            }
         }
     }
 };
