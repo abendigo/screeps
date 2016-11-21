f571ee1ff1898ac81062fc1575927035e2ee191a
Mark Oosterveld <mark@oosterveld.org>
2016-11-21 18:53:18 -0500

    debug

--- role.h2.js
+++ role.h2.js
@@ -1,9 +1,17 @@
 var role = {
+
+	spawn: function() {
+
+	},
+
     /** @param {Creep} creep **/
     run: function(creep) {
     	console.log('h2', creep.name);
 
-
+        var sources = creep.room.find(FIND_SOURCES);
+        for (var i = 0; i < sources.length; i++) {
+        	console.log('id:', sources[i].id);
+        }
     }
 };
 
