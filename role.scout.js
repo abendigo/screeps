8509c9f0665c410a2deb32b141dba0a9a84407db
Mark Oosterveld <mark@oosterveld.org>
2016-11-20 20:13:31 -0500

    2

--- role.scout.js
+++ role.scout.js
@@ -16,21 +16,25 @@ var roleScout = {
             creep.say('delivering');
         }
 
-    	// creep.moveTo(Game.flags.Flag2);
         if (creep.memory.deliver) {
         	var x = 'W63S24';
 
-            var targets = Game.rooms[x].find(FIND_STRUCTURES, {
-                    filter: (structure) => {
-                        return (structure.structureType == STRUCTURE_EXTENSION || structure.structureType == STRUCTURE_SPAWN) &&
-                            structure.energy < structure.energyCapacity;
-                    }
-            });
-            if(targets.length > 0) {
-                if(creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-                    creep.moveTo(targets[0]);
-                }
-            }
+        	if (creep.room.name != 'x') {
+		    	creep.moveTo(Game.flags.Flag2);
+        	} else {
+
+	            var targets = Game.rooms[x].find(FIND_STRUCTURES, {
+	                    filter: (structure) => {
+	                        return (structure.structureType == STRUCTURE_EXTENSION || structure.structureType == STRUCTURE_SPAWN) &&
+	                            structure.energy < structure.energyCapacity;
+	                    }
+	            });
+	            if(targets.length > 0) {
+	                if(creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+	                    creep.moveTo(targets[0]);
+	                }
+	            }
+	        }
         } else {
 
 			var roomName = 'W63S23';
