debd53a327507dce2b74995f98624131213c37c0
Mark Oosterveld <mark@oosterveld.org>
2016-11-20 20:05:31 -0500

    scout

--- role.scout.js
+++ role.scout.js
@@ -4,7 +4,17 @@ var roleScout = {
     run: function(creep) {
     	console.log('roleScout', creep.room.name);
 
-    	creep.moveTo(Game.flags.Flag2);
+    	// creep.moveTo(Game.flags.Flag2);
+
+		var roomName = 'W63S23';
+    	var sources = Game.rooms[roomName].find(FIND_SOURCES);
+    	console.log('sources', sources.length);
+
+        // var sources = creep.room.find(FIND_SOURCES);
+        if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
+            creep.moveTo(sources[0]);
+        }
+
 	}
 };
 
