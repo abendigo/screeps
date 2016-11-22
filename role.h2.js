e829f1f615bac4acc2b1488e6c6b005246717830
Mark Oosterveld <mark@oosterveld.org>
2016-11-21 20:18:21 -0500

    fix it

--- role.h2.js
+++ role.h2.js
@@ -1,18 +1,10 @@
 var role = {
 
 	spawn: function() {
-
+        Game.spawns['home'].createCreep([WORK,WORK,MOVE,MOVE], {role: 'h2'});
 	},
 
-    /** @param {Creep} creep **/
     run: function(creep) {
-        // console.log('h2', creep.name);
-
-    //    var sources = creep.room.find(FIND_SOURCES);
-    //    for (var i = 0; i < sources.length; i++) {
-    //    	console.log('id:', sources[i].id);
-    //    }
-
         var target = Game.getObjectById('57ef9ccc86f108ae6e60cd6e');
         if (creep.harvest(target) == ERR_NOT_IN_RANGE) {
             creep.moveTo(target);
