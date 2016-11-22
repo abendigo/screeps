a067c5d284f563d194a88c1f550626bd2d556c4f
Mark Oosterveld <mark@oosterveld.org>
2016-11-21 20:06:43 -0500

    sweeper

--- main.js
+++ main.js
@@ -51,6 +51,10 @@ module.exports.loop = function () {
             if (energyAvailable >= 300) {
                 Game.spawns[home].createCreep([WORK,WORK,MOVE,MOVE], undefined, {role: 'h2'});
             }
+        } else if (sweepers.length < 2) {
+            if (energyAvailable > 350) {
+                Game.spawns[home].createCreep([CARRY,CARRY,CARRY,MOVE,MOVE,MOVE,MOVE], undefined, {role: 'sweeper'});
+            }
         }
     }
 
