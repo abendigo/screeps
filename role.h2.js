9ad805b275324fa1618b645422012e8e8df66c88
Mark Oosterveld <mark@oosterveld.org>
2016-11-21 19:12:08 -0500

    h2

--- role.h2.js
+++ role.h2.js
@@ -6,12 +6,12 @@ var role = {
 
     /** @param {Creep} creep **/
     run: function(creep) {
-    	console.log('h2', creep.name);
+        // console.log('h2', creep.name);
 
-        var sources = creep.room.find(FIND_SOURCES);
-        for (var i = 0; i < sources.length; i++) {
-        	console.log('id:', sources[i].id);
-        }
+    //    var sources = creep.room.find(FIND_SOURCES);
+    //    for (var i = 0; i < sources.length; i++) {
+    //    	console.log('id:', sources[i].id);
+    //    }
 
         var target = Game.getObjectById('57ef9ccc86f108ae6e60cd6e');
         if (creep.harvest(target) == ERR_NOT_IN_RANGE) {
