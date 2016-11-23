9daadab6d4d89e2ede36b4e9049ffdeda2edf4e7
Mark Oosterveld <mark@oosterveld.org>
2016-11-22 23:16:47 -0500

    towers

--- role.tower.js
+++ role.tower.js
@@ -3,6 +3,13 @@ var role = {
     /** @param {Creep} creep **/
     run: function(tower) {
         console.log('tower', tower.energy)
+
+        let hostile = tower.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
+        if (hostile) {
+            tower.attack(hostile);
+        } else {
+
+        }
     }
 };
 
