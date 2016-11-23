3bbbb9fb027f609451dbc427b51b12678aa60411
Mark Oosterveld <mark@oosterveld.org>
2016-11-22 20:23:54 -0500

    x

--- role.sweeper2.js
+++ role.sweeper2.js
@@ -20,18 +20,6 @@ var role = {
         // Find dropped ENERGY and store it in EXTENSIONS or SPAWN
 
         if (creep.memory.deliver) {
-            let tower = Game.getObjectById('5833230ecbc9367a7f0c0afe');
-            if (tower.energy < tower.energyCapacity) {
-                if (creep.transfer(tower, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-                    creep.moveTo(tower);
-                }
-            } else {
-                var storage = Game.getObjectById('5834d53ba28559d70a076e2c');
-                if (creep.transfer(storage, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-                    creep.moveTo(storage);
-                }
-            }
-
             var target = creep.pos.findClosestByRange(FIND_STRUCTURES, {
                 filter: (structure) => {
                     return (structure.structureType == STRUCTURE_EXTENSION || structure.structureType == STRUCTURE_SPAWN) &&
