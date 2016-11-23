1c9e64e723cc76ad7c77c7242d1f534ca5ece7b9
Mark Oosterveld <mark@oosterveld.org>
2016-11-23 18:18:24 -0500

    debug

--- main.js
+++ main.js
@@ -50,7 +50,9 @@ module.exports.loop = function () {
 
 
     let containers = room.find(FIND_MY_STRUCTURES, {
-        filter: structure => structure.structureType == STRUCTURE_CONTAINER
+        filter: structure => {
+            return structure.structureType == STRUCTURE_CONTAINER;
+        }
     });
     console.log('containers', containers.length)
 
