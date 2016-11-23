efff4dd191e3cba9aa9d74219151c8ae628ac86a
Mark Oosterveld <mark@oosterveld.org>
2016-11-23 18:51:42 -0500

    do some things to assign stuff

--- role.h2.js
+++ role.h2.js
@@ -5,6 +5,27 @@ var role = {
 	},
 
     run: function(creep) {
+        if (creep.memory.container) {
+            console.log('assigned to', creep.memory.container);
+        } else if (creep.room.memory.containers) {
+            console.log('need to pick one');
+
+            let container = creep.pos.findClosestByRange(FIND_STRUCTURES, {
+                filter: structure => {
+                    return structure.structureType == STRUCTURE_CONTAINER &&
+                           creep.room.memory.containers[structure.id] == 'available';
+                }
+            });
+
+            console.log('xxxx', container)
+            if (container) {
+                creep.room.memory.containers[container.id] = creep.id;
+                creep.memory.container = creep.id;
+            }
+        }
+
+
+
         var target = Game.getObjectById('57ef9ccc86f108ae6e60cd6e');
         if (creep.harvest(target) != OK) {
             creep.moveTo(target);
