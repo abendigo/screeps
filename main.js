9b37ec9a07fdee53e04ec45b03ff900be7196666
Mark Oosterveld <mark@oosterveld.org>
2016-11-24 08:12:11 -0500

    upgrade

--- main.js
+++ main.js
@@ -131,7 +131,7 @@ module.exports.loop = function () {
             if (energyAvailable >= 500) {
                 Game.spawns[home].createCreep([WORK,WORK,CARRY,CARRY,MOVE,MOVE,MOVE,MOVE], undefined, {role: 'wallcrew'});
             }
-        } else if (creeps['upgrader'].length < 2) {
+        } else if (creeps['upgrader'].length < 5) {
             if (energyAvailable >= 550) {
                 Game.spawns[home].createCreep([WORK,WORK,WORK,WORK,CARRY,MOVE,MOVE], undefined, {role: 'upgrader'});
             }
