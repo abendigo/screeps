47d1fea89d29c2686331d483ec99711b0c6469b1
Mark Oosterveld <mark@oosterveld.org>
2016-11-23 19:52:04 -0500

    quiet

--- role.h2.js
+++ role.h2.js
@@ -8,35 +8,25 @@ var role = {
         if (creep.fatigue || creep.spawning)
             return;
 
-        console.log(creep.name, 'pos', creep.pos.roomName, creep.pos.x, creep.pos.y);
-
         if (creep.memory.arrived) {
-            console.log(creep.name, 'harvesting')
             let source;
             if (!creep.memory.source) {
-                console.log(creep.name, 'get source')
                 source = creep.pos.findClosestByRange(FIND_SOURCES);
                 creep.memory.source = source.id;
             } else {
-                console.log(creep.name, 'use source')
                 source = Game.getObjectById(creep.memory.source);
             }
 
             creep.harvest(source);
         } else if (creep.memory.container) {
             let target = Game.getObjectById(creep.memory.container);
-        console.log(creep.name, 'pos', creep.pos.roomName, creep.pos.x, creep.pos.y, 'target', target.pos.roomName, target.pos.x, target.pos.y);
 
             if (!(creep.pos.x == target.pos.x && creep.pos.y == target.pos.y)) {
-                console.log(creep.name, 'moving...')
                 creep.moveTo(target);
             } else {
-                console.log(creep.name, '========================================================== arrived')
                 creep.memory.arrived = true;
             }
         } else if (creep.room.memory.containers) {
-            console.log(creep.name, 'need to pick one');
-
             let container = creep.pos.findClosestByRange(FIND_STRUCTURES, {
                 filter: structure => {
                     return structure.structureType == STRUCTURE_CONTAINER &&
@@ -44,22 +34,15 @@ var role = {
                 }
             });
 
-            console.log(creep.name, 'xxxx', container)
             if (container) {
                 creep.room.memory.containers[container.id] = creep.name;
                 creep.memory.container = container.id;
+            } else {
+                console.log(creep.name, 'HELP NO EMPTY CONTIANERS');
             }
         } else {
             console.log(creep.name, 'HELP NOTHING TO DO')
         }
-
-
-
-        // var target = Game.getObjectById('57ef9ccc86f108ae6e60cd6e');
-        // if (creep.harvest(target) != OK) {
-        //     let rc = creep.moveTo(target, {reusePath: 5});
-        //     console.log('rc', rc)
-        // }
     }
 };
 
