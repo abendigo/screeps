2ccbd22f1667a33fe67892a21cb53e24c8b23bb0
Mark Oosterveld <mark@oosterveld.org>
2016-11-24 01:06:59 -0500

    move nergy faster

--- main.js
+++ main.js
@@ -100,7 +100,7 @@ module.exports.loop = function () {
             if (energyAvailable >= 550) {
                 Game.spawns[home].createCreep([WORK,WORK,WORK,WORK,CARRY,MOVE,MOVE], undefined, {role: 'harvester'});
             }
-        } else if (creeps['sweeper'].length < 2) {
+        } else if (creeps['sweeper'].length < 3) {
             if (energyAvailable > 350) {
                 Game.spawns[home].createCreep([CARRY,CARRY,CARRY,MOVE,MOVE,MOVE,MOVE], undefined, {role: 'sweeper'});
             }
