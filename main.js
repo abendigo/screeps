11fa1b4c55a284ee66d6a9a45072f51da5d0e93e
Mark Oosterveld <mark@oosterveld.org>
2016-11-23 19:09:27 -0500

    more debug

--- main.js
+++ main.js
@@ -67,10 +67,11 @@ module.exports.loop = function () {
     console.log('memory.containers', JSON.stringify(room.memory.containers));
 
     for (let x of containers) {
-        console.log('--- container', x.id)
         if (room.memory.containers[x.id]) {
-            if (room.memory.containers[x.id] != 'available') {
-                console.log('assigned to', room.memory.containers[x.id])
+            if (room.memory.containers[x.id] == 'available') {
+                console.log('--- container', x.id, 'available')
+            } else {
+                console.log('--- container', x.id,'assigned to', room.memory.containers[x.id])
                 if (Game.creeps[room.memory.containers[x.id]]) {
                     console.log('still alive')
                 } else {
