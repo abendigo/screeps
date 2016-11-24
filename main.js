863979509875bb3d048a9d1020dd1ebf60d6a873
Mark Oosterveld <mark@oosterveld.org>
2016-11-24 01:18:42 -0500

    cleanup

--- main.js
+++ main.js
@@ -60,11 +60,10 @@ module.exports.loop = function () {
     console.log('memory.containers', JSON.stringify(room.memory.containers));
 
     for (let j in room.memory.containers) {
-        console.log('j', j);
-        // let y = Game.getObjectById(j);
-        // if (!y) {
-        //     delete room.memory.containers[j];
-        // }
+        let y = Game.getObjectById(j);
+        if (!y) {
+            delete room.memory.containers[j];
+        }
     }
 
     for (let x of containers) {
