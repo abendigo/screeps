b40d6378d7b6e4bb2894e9fe16d70ab67fca3b93
Mark Oosterveld <mark@oosterveld.org>
2016-11-24 21:50:15 -0500

    wall crew

--- main.js
+++ main.js
@@ -123,10 +123,10 @@ module.exports.loop = function () {
             if (energyAvailable >= 500) {
                 Game.spawns[home].createCreep([WORK,WORK,CARRY,CARRY,MOVE,MOVE,MOVE,MOVE], undefined, {role: 'roadcrew'});
             }
-        // } else if (creeps['wallcrew'].length < 1) {
-        //     if (energyAvailable >= 500) {
-        //         Game.spawns[home].createCreep([WORK,WORK,CARRY,CARRY,MOVE,MOVE,MOVE,MOVE], undefined, {role: 'wallcrew'});
-        //     }
+        } else if (creeps['wallcrew'].length < 1) {
+            if (energyAvailable >= 500) {
+                Game.spawns[home].createCreep([WORK,WORK,CARRY,CARRY,MOVE,MOVE,MOVE,MOVE], undefined, {role: 'wallcrew'});
+            }
         } else if (creeps['upgrader'].length < 2) {
             if (energyAvailable >= 550) {
                 Game.spawns[home].createCreep([WORK,WORK,WORK,WORK,CARRY,MOVE,MOVE], undefined, {role: 'upgrader'});
