6b546bf13c5aa387ff3880ecbfb18692e6a34e1e
Mark Oosterveld <mark@oosterveld.org>
2016-11-26 11:00:59 -0500

    debug

--- role.room.js
+++ role.room.js
@@ -5,7 +5,7 @@ var role = {
         console.log(`Room "${room.name}" has ${room.energyAvailable} energy of ${room.energyCapacityAvailable}`);
 
         let creeps = {};
-        let output = `${room.anme}: `;
+        let output = `${room.name}: `;
         for (var role in roles) {
             creeps[role] = _.filter(Game.creeps, (creep) => creep.memory.role == role && creep.room.name == room.name);
             output += `${role}: ${creeps[role].length} `;
