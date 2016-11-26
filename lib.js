c3c9ff1b900a12ac57ff3bf106b76acd61a31931
Mark Oosterveld <mark@oosterveld.org>
2016-11-26 14:20:14 -0500

    leave spawn alone

--- lib.js
+++ lib.js
@@ -1,22 +1,38 @@
 let lib = {
     refuel: (creep) => {
         console.log('refuel', creep.name)
-        var container = creep.pos.findClosestByRange(FIND_STRUCTURES, {
-            filter: (structure) => {
-                return (structure.structureType == STRUCTURE_CONTAINER && structure.store[RESOURCE_ENERGY] >= 50) ||
-                       (structure.structureType == STRUCTURE_EXTENSION && structure.energy >= 50) ||
-                       (structure.structureType == STRUCTURE_SPAWN && structure.energy >= 50);
-            }
-        });
 
-        if (container) {
-            console.log('found', container.id, container.structureType)
-            if (creep.withdraw(container, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(container);
+        if (creep.room.energyCapacityAvailable < 550) {
+            var container = creep.pos.findClosestByRange(FIND_STRUCTURES, {
+                filter: (structure) => {
+                    return (structure.structureType == STRUCTURE_CONTAINER && structure.store[RESOURCE_ENERGY] >= 50) ||
+                        (structure.structureType == STRUCTURE_EXTENSION && structure.energy >= 50);
+                }
+            });
+            if (container) {
+                console.log('found', container.id, container.structureType)
+                if (creep.withdraw(container, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(container);
+                }
             }
         } else {
-            console.log('not found. parking');
-            creep.moveTo(Game.flags.parking);
+            var container = creep.pos.findClosestByRange(FIND_STRUCTURES, {
+                filter: (structure) => {
+                    return (structure.structureType == STRUCTURE_CONTAINER && structure.store[RESOURCE_ENERGY] >= 50) ||
+                        (structure.structureType == STRUCTURE_EXTENSION && structure.energy >= 50) ||
+                        (structure.structureType == STRUCTURE_SPAWN && structure.energy >= 50);
+                }
+            });
+
+            if (container) {
+                console.log('found', container.id, container.structureType)
+                if (creep.withdraw(container, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(container);
+                }
+            } else {
+                console.log('not found. parking');
+                creep.moveTo(Game.flags.parking);
+            }
         }
     }
 };
