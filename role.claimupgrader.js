c65222d9dcc4d7a0b8e5d8c9693952569e369fee
Mark Oosterveld <mark@oosterveld.org>
2016-11-26 11:10:26 -0500

    upgrade claim

--- role.claimupgrader.js
+++ role.claimupgrader.js
@@ -16,14 +16,23 @@ var role = {
             creep.say('upgrading');
         }
 
-        if (!creep.memory.upgrading) {
-            let sources = creep.room.find(FIND_SOURCES);
-            if(creep.harvest(sources[1]) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(sources[1]);
+        if (creep.room.name === 'W63S24') {  // Home
+            if (creep.memory.building) {
+                let exit = creep.pos.findClosestByRange(FIND_EXIT_TOP);
+                creep.moveTo(exit);
+            } else {
+                lib.refuel(creep);
             }
-        } else {
-            if (creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(creep.room.controller);
+        } else if (creep.room.name === 'W63S23') {  // Target
+            if (!creep.memory.upgrading) {
+                let sources = creep.room.find(FIND_SOURCES);
+                if(creep.harvest(sources[1]) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(sources[1]);
+                }
+            } else {
+                if (creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(creep.room.controller);
+                }
             }
         }
     }
