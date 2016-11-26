3d113967ee4682286420e2d393d3d1718691b010
Mark Oosterveld <mark@oosterveld.org>
2016-11-26 11:21:59 -0500

    clean up old roads

--- role.room.js
+++ role.room.js
@@ -4,13 +4,13 @@ var role = {
     run: function(room, roles) {
         console.log(`Room "${room.name}" has ${room.energyAvailable} energy of ${room.energyCapacityAvailable}`);
 
-        let roads = room.find(FIND_CONSTRUCTION_SITES, {
-            filter: structure => structure.structureType == STRUCTURE_ROAD
-        });
-        console.log('roads', roads.length);
-        for (road of roads) {
-            road.remove();
-        }
+        // let roads = room.find(FIND_CONSTRUCTION_SITES, {
+        //     filter: structure => structure.structureType == STRUCTURE_ROAD
+        // });
+        // console.log('roads', roads.length);
+        // for (road of roads) {
+        //     road.remove();
+        // }
 
         let creeps = {};
         let output = `${room.name}: `;
