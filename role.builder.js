320da87731905a91630dd491208838580367cbda
Mark Oosterveld <mark@oosterveld.org>
2016-11-26 14:55:52 -0500

    x

--- role.builder.js
+++ role.builder.js
@@ -41,11 +41,14 @@ let roleBuilder = {
             }
 	    }
 	    else {
-            lib.refuel(creep);
-        // var sources = creep.room.find(FIND_SOURCES);
-         //    if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
-         //        creep.moveTo(sources[0]);
-         //    }
+            if (creep.room.energyCapacityAvailable > 550) {
+                var sources = creep.room.find(FIND_SOURCES);
+                if (creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(sources[0]);
+                }
+            } else {
+                lib.refuel(creep);
+            }
 /*
             var container = creep.pos.findClosestByRange(FIND_STRUCTURES, {
                 filter: (structure) => {
