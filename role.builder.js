0f5e96a788d0f78cdec4324c7d7474399a09fd6c
Mark Oosterveld <mark@oosterveld.org>
2016-11-26 18:29:56 -0500

    hack

--- role.builder.js
+++ role.builder.js
@@ -42,9 +42,10 @@ let roleBuilder = {
 	    }
 	    else {
             if (creep.room.energyCapacityAvailable < 550) {
-                var sources = creep.room.find(FIND_SOURCES);
-                if (creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
-                    creep.moveTo(sources[0]);
+                let source = Game.getObjectById('57ef9ccb86f108ae6e60cd6b');
+                // var sources = creep.room.find(FIND_SOURCES);
+                if (creep.harvest(source) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(source);
                 }
             } else {
                 lib.refuel(creep);
