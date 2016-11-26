371e3afa08124d3b6184f83ee57a5c098f5c1aec
Mark Oosterveld <mark@oosterveld.org>
2016-11-26 15:04:31 -0500

    build

--- role.builder.js
+++ role.builder.js
@@ -41,7 +41,7 @@ let roleBuilder = {
             }
 	    }
 	    else {
-            if (creep.room.energyCapacityAvailable > 550) {
+            if (creep.room.energyCapacityAvailable < 550) {
                 var sources = creep.room.find(FIND_SOURCES);
                 if (creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
                     creep.moveTo(sources[0]);
