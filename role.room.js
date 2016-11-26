3843c66059c5cfeb2764f25ab1a092851309193d
Mark Oosterveld <mark@oosterveld.org>
2016-11-26 18:34:05 -0500

    sweep

--- role.room.js
+++ role.room.js
@@ -86,6 +86,10 @@ var role = {
                 if (room.energyAvailable >= 300) {
                     spawn.createCreep([WORK,WORK,CARRY,MOVE], undefined, {role: 'builder'});
                 }
+            } else if (creeps['litter'].length < 2) {
+                if (room.energyAvailable > 300) {
+                    spawn.createCreep([CARRY,CARRY,MOVE,MOVE,MOVE,MOVE], undefined, {role: 'litter'});
+                }
             }
         } else {
             if (creeps['h2'].length < containers.length) {
@@ -100,7 +104,7 @@ var role = {
                 if (room.energyAvailable > 350) {
                     spawn.createCreep([CARRY,CARRY,CARRY,MOVE,MOVE,MOVE,MOVE], undefined, {role: 'sweeper'});
                 }
-            } else if (creeps['sweeper2'].length < 4) {
+            } else if (creeps['litter'].length < 4) {
                 if (room.energyAvailable > 350) {
                     spawn.createCreep([CARRY,CARRY,CARRY,MOVE,MOVE,MOVE,MOVE], undefined, {role: 'sweeper2'});
                 }
