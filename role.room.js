a34aaf2e9a35384108f11f0e2aa9f15c3b0c6fc1
Mark Oosterveld <mark@oosterveld.org>
2016-11-27 18:08:04 -0500

    argh

--- role.room.js
+++ role.room.js
@@ -130,8 +130,7 @@ var role = {
                 if (room.energyAvailable >= 300) {
                     var name = spawn.createCreep([WORK,WORK,CARRY,MOVE], undefined, {role: 'harvester'});
                 }
-      
-            else if (creeps['h2'].length < containers.length) {
+            } else if (creeps['h2'].length < containers.length) {
                 if (room.energyAvailable >= 300) {
                     spawn.createCreep([WORK,WORK,MOVE,MOVE], undefined, {role: 'h2'});
                 }
