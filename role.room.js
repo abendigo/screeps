7c3a92ea2b636139c35ff65f75c034a4b10ffc17
Mark Oosterveld <mark@oosterveld.org>
2016-11-28 17:51:02 -0500

    sweep

--- role.room.js
+++ role.room.js
@@ -214,10 +214,10 @@ var role = {
             //     if (energyAvailable >= 550) {
             //         Game.spawns[home].createCreep([WORK,WORK,WORK,WORK,CARRY,MOVE,MOVE], undefined, {role: 'harvester'});
             //     }
-            } else if (creeps['sweeper'].length < 2) {
-                if (room.energyAvailable > 350) {
-                    spawn.createCreep([CARRY,CARRY,CARRY,MOVE,MOVE,MOVE,MOVE], undefined, {role: 'sweeper'});
-                }
+            // } else if (creeps['sweeper'].length < 2) {
+            //     if (room.energyAvailable > 350) {
+            //         spawn.createCreep([CARRY,CARRY,CARRY,MOVE,MOVE,MOVE,MOVE], undefined, {role: 'sweeper'});
+            //     }
             } else if (creeps['litter'].length < 2) {
                 if (room.energyAvailable > 350) {
                     spawn.createCreep([CARRY,CARRY,CARRY,MOVE,MOVE,MOVE,MOVE], undefined, {role: 'litter'});
