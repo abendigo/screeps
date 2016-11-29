d97dbbf5aeb101fa84494eba7c8bf3457d9ed607
Mark Oosterveld <mark@oosterveld.org>
2016-11-28 19:06:12 -0500

    more h3

--- role.h3.js
+++ role.h3.js
@@ -7,12 +7,7 @@ var role = {
         if (creep.fatigue || creep.spawning)
             return;
 
-        if (creep.memory.source) {
-console.log('1')
-            let target = Game.getObjectById(creep.memory.source);
-            console.log('target', target);
-        } else if (creep.room.memory.sources) {
-console.log('2')            
+        if (!creep.memory.source && creep.room.memory.sources) {
             let source = creep.pos.findClosestByRange(FIND_SOURCES, {
                 filter: structure => {
                     return creep.room.memory.sources[structure.id] == 'available';
@@ -20,13 +15,17 @@ console.log('2')
             });
 
             if (source) {
-console.log('3')                
                 creep.room.memory.sources[source.id] = creep.name;
                 creep.memory.source = source.id;
             } else {
                 console.log(creep.name, 'HELP NO EMPTY SOURCES');
                 creep.suicide();
             }
+        }
+
+        if (creep.memory.source) {
+            let target = Game.getObjectById(creep.memory.source);
+            console.log('target', target);
         }
     }
 };
