752eceb0f78a8bf2a18d14808c54cb75328df119
Mark Oosterveld <mark@oosterveld.org>
2016-11-29 23:18:22 -0500

    use storage again

--- role.upgrader.js
+++ role.upgrader.js
@@ -26,6 +26,7 @@ var roleUpgrader = {
         if (!creep.memory.upgrading) {
             let container = creep.pos.findClosestByRange(FIND_STRUCTURES, {
                 filter: (structure) => {
+                    console.log('type', structure.structureType, STRUCTURE_STORAGE, structure.energy)
                     return structure.structureType == STRUCTURE_STORAGE && structure.energy >= 50;
                 }
             });
