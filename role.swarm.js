a73800dd25861c0dd24ea20858e7d89bf1052f90
Mark Oosterveld <mark@oosterveld.org>
2016-12-01 18:12:41 -0500

    swarm

--- role.swarm.js
+++ role.swarm.js
@@ -6,9 +6,25 @@ var role = {
 
         if (creep.fatigue || creep.spawning)
             return;
+
+        
     
-
-
+        if (creep.room.name === 'W63S24') {  // Home
+            let exit = creep.pos.findClosestByRange(FIND_EXIT_RIGHT);
+            creep.moveTo(exit);
+        } else if (creep.room.name === 'W62S24') {
+            let exit = creep.pos.findClosestByRange(FIND_EXIT_NORTH);
+            creep.moveTo(exit);
+        } else if (creep.room.name === 'W62S23') {
+            let exit = creep.pos.findClosestByRange(FIND_EXIT_NORTH);
+            creep.moveTo(exit);
+        } else if (creep.room.name === 'W62S22') {
+            let spawn = creep.pos.findClosestByRange(FIND_HOSTILE_SPAWNS);
+            if (spawn) {
+                if (creep.attack(spawn) == ERR_NOT_IN_RANGE)
+                    creep.moveTo(spawn);
+                }              
+        }
     }
 };
 
