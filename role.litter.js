3c4cdea52b2c796bd232a23724e68290f3796542
Mark Oosterveld <mark@oosterveld.org>
2016-11-30 21:20:24 -0500

    x

--- role.litter.js
+++ role.litter.js
@@ -23,10 +23,20 @@ var role = {
 
         // Find dropped ENERGY and store it in EXTENSIONS or SPAWN
         if (creep.memory.state === 'deliver') {
+                var storage = creep.pos.findClosestByRange(FIND_STRUCTURES, {
+                    filter: structure => structure.structureType === STRUCTURE_STORAGE
+                });
+                if (storage) {
+                    if (creep.transfer(storage, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                        creep.moveTo(storage);
+                    }
+                }
+            
             var target = creep.pos.findClosestByRange(FIND_STRUCTURES, {
                 filter: (structure) => {
-                    return (structure.structureType == STRUCTURE_EXTENSION || structure.structureType == STRUCTURE_SPAWN) &&
-                        structure.energy < structure.energyCapacity;
+                    return (structure.structureType == STRUCTURE_EXTENSION && structure.energy < structure.energyCapacity) || 
+                           (structure.structureType == STRUCTURE_SPAWN && structure.energy < structure.energyCapacity) || 
+                           (structure => structure.structureType === STRUCTURE_STORAGE);
                 }
             });
 
