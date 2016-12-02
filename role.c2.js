24c4172b207f6b295e64c6d9de4ac71aac25739f
Mark Oosterveld <mark@oosterveld.org>
2016-12-01 20:53:58 -0500

    respawn

--- role.c2.js
+++ role.c2.js
@@ -7,12 +7,16 @@ var role = {
 
         console.log(`${creep.name}@${creep.room.name}:${creep.memory.role}[${creep.memory.state}] ${creep.memory.target} ${creep.ticksToLive}`)
 
-        if (!creep.memory.target)
-            creep.memory.target = '57ef9ce186f108ae6e60cf37';
+        // if (!creep.memory.target)
+        //     creep.memory.target = '57ef9ce186f108ae6e60cf37';
 
-        let claim = Game.getObjectById(creep.memory.target)
-        if (creep.reserveController(claim) == ERR_NOT_IN_RANGE)
-            creep.moveTo(claim);
+        if (creep.memory.target) {
+            let claim = Game.getObjectById(creep.memory.target)
+            if (claim)
+                console.log('claim', claim);
+        }
+        // if (creep.reserveController(claim) == ERR_NOT_IN_RANGE)
+        //     creep.moveTo(claim);
     }
 };
 
